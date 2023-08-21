1956145945c994318e18fc988b2feabf40a5977b
pszczepkowski <p.szczepkowski87@gmail.com>
2023-08-21 20:52:18 +0200

    9. dodać metodę która zsumuje n liczb integer

--- src/main/java/pl/com/coders/biedronka2/Calculator.java
+++ src/main/java/pl/com/coders/biedronka2/Calculator.java
@@ -1,12 +1,37 @@
 package pl.com.coders.biedronka2;
 
+import java.util.List;
+
 public class Calculator {
 
-    public int sum (int a, int b){
-        return a+b;
+    private int value = 0;
+
+    public int sum(int a, int b) {
+        return a + b;
     }
 
-    public int subtract (int a, int b){
-        return a-b;
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public float divide(float a, float b) {
+        return a / b;
+    }
+
+
+    public int sum(List<Integer> list) {
+
+        int sum = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            Integer integer = list.get(i);
+            sum = sum + integer;
+        }
+
+        return sum;
     }
 }
--- src/test/java/pl/com/coders/biedronka2/CalculatorTest.java
+++ src/test/java/pl/com/coders/biedronka2/CalculatorTest.java
@@ -1,6 +1,10 @@
 package pl.com.coders.biedronka2;
 
 import org.junit.jupiter.api.Test;
+import pl.com.coders.biedronka2.Calculator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,16 +13,45 @@ class CalculatorTest {
     @Test
     void sum() {
         Calculator calculator = new Calculator();
-        int sum = calculator.sum(2,5);
+        int sum = calculator.sum(Integer.MAX_VALUE, Integer.MAX_VALUE);
 
-        assertEquals(7, sum);
+        assertEquals(-2, sum);
     }
 
     @Test
     void subtract() {
         Calculator calculator = new Calculator();
-        int subtract = calculator.subtract(7,5);
+        int subtract = calculator.subtract(7, 5);
 
         assertEquals(2, subtract);
+    }
+
+    @Test
+    void testSum() {
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1234);
+        integers.add(1212312);
+        integers.add(123232);
+
+        Calculator calculator = new Calculator();
+        int sum = calculator.sum(integers);
+
+        assertEquals(1336778, sum);
+    }
+
+    @Test
+    void multiply() {
+        Calculator calculator = new Calculator();
+        int subtract = calculator.multiply(7, 5);
+
+        assertEquals(35, subtract);
+    }
+
+    @Test
+    void divide() {
+        Calculator calculator = new Calculator();
+        float subtract = calculator.divide(7, 2);
+
+        assertEquals(3.5, subtract);
     }
 }
